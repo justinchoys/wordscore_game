50e6c61c1b63fc0e7b9bcf4657b9f4e407116b2f
justinchoys <justinchoys@gmail.com>
2018-08-29 13:33:50 -0400

    Clean up wordscore_self script

--- wordscore_self.py
+++ wordscore_self.py
@@ -86,8 +86,8 @@ def displayHand(hand):
     """
     for letter in hand.keys():
         for j in range(hand[letter]):
-             print(letter,end=" ")       # print all on the same line
-#    print()                             # print an empty line
+             print(letter,end=" ")
+
 
 
 def dealHand(n):
